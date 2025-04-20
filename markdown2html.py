f5b517df89699a732467d8ecefbfe70ca99289c4
Swagtiago29 <sant29ramos@gmail.com>
2025-04-20 15:51:54 -0300

    0

--- markdown2html.py
+++ markdown2html.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python3
 import sys
 import os
 
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
 
-elif not os.path.isfile(sys.argv[1]):
-    print(f"Missing {sys.argv[1]}", file=sys.stderr)
-    sys.exit(1)
-sys.exit(0)
+    if not os.path.isfile(sys.argv[1]):
+        print(f"Missing {sys.argv[1]}", file=sys.stderr)
+        sys.exit(1)
+    sys.exit(0)
+

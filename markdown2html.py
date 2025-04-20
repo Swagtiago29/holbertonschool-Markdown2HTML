2c071c978920bcc60b3c8f463576f2fd07b2cbbb
Swagtiago29 <sant29ramos@gmail.com>
2025-04-20 15:26:24 -0300

    add 0

---
+++ markdown2html.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+print(sys.argv[0])
+print(sys.argv[1])
+print(sys.argv[2])
+if len(sys.argv) < 3:
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+    sys.exit(1)
+
+if not os.path.isfile(sys.argv[1]):
+    print(f"Missing {sys.argv[1]}", file=sys.stderr)
+    sys.exit(1)
+sys.exit(0)

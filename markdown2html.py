91b11b38f453995482c1a53b3ebe228bf768ed41
Swagtiago29 <sant29ramos@gmail.com>
2025-04-20 15:28:10 -0300

    add 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,11 +1,15 @@
 import sys
 import os
 
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
 
-if not os.path.isfile(sys.argv[1]):
-    print(f"Missing {sys.argv[1]}", file=sys.stderr)
-    sys.exit(1)
-sys.exit(0)
+    if not os.path.isfile(sys.argv[1]):
+        print(f"Missing {sys.argv[1]}", file=sys.stderr)
+        sys.exit(1)
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()

0201099cf6975cb9d27adb5be5601580b8c052cc
Swagtiago29 <sant29ramos@gmail.com>
2025-04-20 15:34:42 -0300

    0

--- markdown2html.py
+++ markdown2html.py
@@ -6,7 +6,7 @@ if len(sys.argv) < 3:
     print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     sys.exit(1)
 
-if not os.path.isfile(sys.argv[1]):
+elif not os.path.isfile(sys.argv[1]):
     print(f"Missing {sys.argv[1]}", file=sys.stderr)
     sys.exit(1)
 sys.exit(0)

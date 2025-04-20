c15a7f462821c1c2e8fb54a600c491f8724a583f
Swagtiago29 <sant29ramos@gmail.com>
2025-04-20 15:27:10 -0300

    0

--- markdown2html.py
+++ markdown2html.py
@@ -1,9 +1,6 @@
 import sys
 import os
 
-print(sys.argv[0])
-print(sys.argv[1])
-print(sys.argv[2])
 if len(sys.argv) < 3:
     print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     sys.exit(1)
